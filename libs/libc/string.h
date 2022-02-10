0f7e149600a3d1f1c513bc901e077020b972baed
rickypurogu <tanas.riccardo03@gmail.com>
2022-02-10 19:44:48 +0100

    Added strcpy prototype

--- libs/libc/string.h
+++ libs/libc/string.h
@@ -6,6 +6,7 @@
 unsigned int strlen(const char str[]);
 bool strcmp(const char* s1, const char* s2);
 int32_t strncmp(const char* s1, const char* s2, size_t len);
+bool strcpy(const char *src, char *dest);
 char* strf(const char* str, char buffer[], ...);
 char* vstrf(const char* istr, char buffer[], va_list args);
 char* itoa(unsigned i, unsigned base, char* buf);
