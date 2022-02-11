2699f8dc4525d3ee233842752403621ab9f1e81b
rickypurogu <tanas.riccardo03@gmail.com>
2022-02-11 17:49:59 +0100

    Added Task definition

---
+++ kernel/common/tasks/task.c
@@ -0,0 +1,42 @@
+#include "task.h"
+#include "context.h"
+#include "../kservice.h"
+#include "../memory/memory.h"
+#include "../memory/space.h"
+#include <liballoc.h>
+#include <string.h>
+
+// === PRIVATE FUNCTIONS ========================
+
+void task_set_stack(Task* task) {
+    task->stack = (uintptr_t)kmalloc(PROCESS_STACK_SIZE);
+    space_map(task->space, task->stack, PROCESS_STACK_SIZE, PROCESS_STACK_BASE);
+}
+
+// === PUBLIC FUNCTIONS =========================
+
+Task* NewTask(const char* name, bool user) {
+    Task* task = (Task*)kmalloc(sizeof(Task));
+
+    if (strlen(name) > TASK_NAME_MAX) {
+        ks.warn("Task name is longer than the maximum allowed (%i). The name will be truncated.", TASK_NAME_MAX);
+        memory_copy(name, task->name, TASK_NAME_MAX);
+    } else {
+        strcpy(name, task->name);
+    }
+
+    task->status = TASK_EMBRYO;
+    task->user = user;
+    task->lock = NewLock;
+    task->space = NewSpace();
+    task->context = NewContext();
+
+    task_set_stack(task);
+    return task;
+}
+
+void DestroyTask(Task* task) {
+    DestroySpace(task->space);
+    DestroyContext(task->context);
+    kfree(task);
+}
---
+++ kernel/common/tasks/task.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <neutrino/lock.h>
+#include <_null.h>
+#include "context.h"
+#include "../memory/space.h"
+
+typedef enum __task_status {
+    TASK_EMBRYO,            // task is creating its Context and Space
+    TASK_READY,             // task is ready for execution
+    TASK_RUNNING,           // task is running
+    TASK_IO,                // task is blocked by I/O 
+    TASK_SYSCALL,           // task is executing a syscall
+    TASK_ZOMBIE             // task is ended and freeing its resources
+} TaskStatus;
+
+#define TASK_NAME_MAX 64
+#define PROCESS_STACK_SIZE  0x4000
+#define PROCESS_STACK_BASE  0xc0000000
+
+typedef struct __task {
+    uint32_t pid;
+    char name[TASK_NAME_MAX];
+    TaskStatus status;
+
+    Lock lock;
+    bool user;
+    Context* context;
+    Space* space;
+
+    uintptr_t stack;
+} Task;
+
+#define IsTaskRunnable(task)    ((task)->status == TASK_READY || (task)->status == TASK_SYSCALL)
+
+Task* NewTask(const char* name, bool user);
+void DestroyTask(Task* task);
