a3e7cff9687d31d5703e08afb8133cff4091f1a8
rickypurogu <tanas.riccardo03@gmail.com>
2022-02-22 11:27:45 +0100

    Removed idle Task

--- kernel/common/cpu.h
+++ kernel/common/cpu.h
@@ -3,22 +3,21 @@
 #include <stdint.h>
 #include "tasks/task.h"
 
-#ifdef __x86_64
-#include "kernel/x86_64/smp.h"
-#else
-#error "Unsupported platform"
-#endif
-
 #define MAX_CPU 64
 #define CPU_STACK_SIZE      0x2000
 
 struct __tasks {
-    Task* idle;
     Task* current;
     Task* next;
 };
 
 typedef struct __cpu Cpu;
+
+#ifdef __x86_64
+#include "kernel/x86_64/smp.h"
+#else
+#error "Unsupported platform"
+#endif
 
 Cpu* get_cpu(uint32_t id);
 Cpu* get_current_cpu();
