8efafd9d10507ab9e8093e9eee1dafc0157025a9
rickypurogu <tanas.riccardo03@gmail.com>
2022-06-01 21:49:49 +0200

    Changes to atomic.c

--- libs/neutrino/atomic.c
+++ libs/neutrino/atomic.c
@@ -6,45 +6,45 @@
 // --- ATOMIC SET -------------------------------
 
 void unoptimized atomic_set_qword(uintptr_t ptr, uint64_t value) {
-    __atomic_store_n((uint64_t*)ptr, value, __ATOMIC_SEQ_CST);
+    __atomic_store_n((volatile uint64_t*)ptr, value, __ATOMIC_SEQ_CST);
 }
 
 void unoptimized atomic_set_dword(uintptr_t ptr, uint32_t value) {
-    __atomic_store_n((uint32_t*)ptr, value, __ATOMIC_SEQ_CST);
+    __atomic_store_n((volatile uint32_t*)ptr, value, __ATOMIC_SEQ_CST);
 }
 
 void unoptimized atomic_set_word(uintptr_t ptr, uint16_t value) {
-    __atomic_store_n((uint16_t*)ptr, value, __ATOMIC_SEQ_CST);
+    __atomic_store_n((volatile uint16_t*)ptr, value, __ATOMIC_SEQ_CST);
 }
 
 void unoptimized atomic_set_byte(uintptr_t ptr, uint8_t value) {
-    __atomic_store_n((uint8_t*)ptr, value, __ATOMIC_SEQ_CST);
+    __atomic_store_n((volatile uint8_t*)ptr, value, __ATOMIC_SEQ_CST);
 }
 
 // --- ATOMIC READ ------------------------------
 
 uint64_t unoptimized atomic_get_qword(uintptr_t ptr) {
-    return __atomic_load_n((uint64_t*)ptr, __ATOMIC_SEQ_CST);
+    return __atomic_load_n((const volatile uint64_t*)ptr, __ATOMIC_SEQ_CST);
 }
 
 uint32_t unoptimized atomic_get_dword(uintptr_t ptr) {
-    return __atomic_load_n((uint32_t*)ptr, __ATOMIC_SEQ_CST);
+    return __atomic_load_n((const volatile uint32_t*)ptr, __ATOMIC_SEQ_CST);
 }
 
 uint16_t unoptimized atomic_get_word(uintptr_t ptr) {
-    return __atomic_load_n((uint16_t*)ptr, __ATOMIC_SEQ_CST);
+    return __atomic_load_n((const volatile uint16_t*)ptr, __ATOMIC_SEQ_CST);
 }
 
 uint8_t unoptimized atomic_get_byte(uintptr_t ptr) {
-    return __atomic_load_n((uint8_t*)ptr, __ATOMIC_SEQ_CST);
+    return __atomic_load_n((const volatile uint8_t*)ptr, __ATOMIC_SEQ_CST);
 }
 
 // --- ATOMIC LOCK ------------------------------
 
 bool unoptimized atomic_test_and_set(uint8_t* ptr) {
-    return __atomic_test_and_set(ptr, __ATOMIC_ACQUIRE);
+    return __atomic_test_and_set((volatile uint8_t*)ptr, __ATOMIC_ACQUIRE);
 }
 
 void unoptimized atomic_release(uint8_t* ptr) {
-    __atomic_clear(ptr, __ATOMIC_RELEASE);
+    __atomic_clear((volatile uint8_t*)ptr, __ATOMIC_RELEASE);
 }
