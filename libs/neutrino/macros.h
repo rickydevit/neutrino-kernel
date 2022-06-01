6fbb72245966d846c342f3a78626d54d09aeb008
rickypurogu <tanas.riccardo03@gmail.com>
2022-06-01 21:50:03 +0200

    Added new macros

--- libs/neutrino/macros.h
+++ libs/neutrino/macros.h
@@ -3,3 +3,8 @@
 #define unoptimized     __attribute__((optimize("O0")))
 #define packed          __attribute__((packed))
 #define aligned(align)  __attribute__((aligned(align)))
+
+#define Max(a,b)        ((a > b) ? a : b)
+#define Min(a,b)        ((a < b) ? a : b)
+
+#define AlignUp(addr, align)    ((addr + align-1) & ~(align-1))
