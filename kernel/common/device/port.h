c46eb042d1cf53c715d027f47b74b5b4b00d1ba7
rickypurogu <tanas.riccardo03@gmail.com>
2022-06-02 14:23:59 +0200

    Port comms now defined as arch-dependent

--- kernel/common/device/port.h
+++ kernel/common/device/port.h
@@ -1,5 +1,11 @@
 #pragma once
 
+#ifdef __x86_64
+#include "kernel/x86_64/device/port.h"
+#else
+#error "Unsupported platform"
+#endif
+
 extern unsigned char port_byte_in(unsigned short port);
 extern void port_byte_out(unsigned short port, unsigned char data);
 extern unsigned short port_word_in(unsigned short port);
