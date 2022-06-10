f3eaf4df8acea08f94c9c8f81527860ffed09b74
rickypurogu <tanas.riccardo03@gmail.com>
2022-06-10 17:07:11 +0200

    Removed unused display code

--- kernel/x86_64/arch.c
+++ kernel/x86_64/arch.c
@@ -12,7 +12,6 @@
 #include "device/time/hpet.h"
 #include "device/time/rtc.h"
 #include "kernel/common/device/serial.h"
-#include "kernel/common/video/display.h"
 #include "kernel/common/kservice.h"
 #include "kernel/common/tasks/scheduler.h"
 #include "kernel/common/modules.h"
@@ -100,44 +99,6 @@ void unoptimized _kstart(struct stivale2_struct *stivale2_struct) {
     //? -----------------------------------------
     ks.fatal(FatalError(FATAL_ERROR, "Neutrino returned!"));
     for (;;) asm volatile("hlt");
-
-    /*
-    //? Display driver setup
-    struct stivale2_struct_tag_terminal *term_str_tag = stivale2_get_tag(stivale2_struct, STIVALE2_STRUCT_TAG_TERMINAL_ID);
-    struct stivale2_struct_tag_framebuffer *framebuf_str_tag = stivale2_get_tag(stivale2_struct, STIVALE2_STRUCT_TAG_FRAMEBUFFER_ID);
-    uint32_t checker_result = check_framebuffer_or_terminal(framebuf_str_tag, term_str_tag);
-
-    if (checker_result == CHECKER_NEITHER_AVAILABLE) {
-        ks.fatal(FatalError(NO_FRAMEBUFFER, "The machine could neither provide a framebuffer address or a terminal interface. Cannot continue, halting system."));
-
-    } else if (checker_result == CHECKER_FRAMEBUFFER_AVAILABLE) {
-        ks.dbg("Initializing video driver... ");
-        if (init_video_driver(framebuf_str_tag->framebuffer_addr, framebuf_str_tag->framebuffer_width, framebuf_str_tag->framebuffer_height, 
-                        framebuf_str_tag->framebuffer_pitch, framebuf_str_tag->framebuffer_bpp, framebuf_str_tag->red_mask_size, framebuf_str_tag->green_mask_size, 
-                        framebuf_str_tag->blue_mask_size, framebuf_str_tag->red_mask_shift, framebuf_str_tag->green_mask_shift, framebuf_str_tag->blue_mask_shift))
-            ks.dbg("Video driver initialized. Framebuffer at %x", framebuf_str_tag->framebuffer_addr);
-    
-    } else if (checker_result == CHECKER_TERMINAL_AVAILABLE) {
-        void *term_write_ptr = (void *)term_str_tag->term_write;
-        void (*term_write)(const char *string, size_t length) = term_write_ptr;
-
-        term_write("\n \
-        _   _            _        _               _  __                    _  \n \
-        | \\ | |          | |      (_)             | |/ /                   | | \n \
-        |  \\| | ___ _   _| |_ _ __ _ _ __   ___   | ' / ___ _ __ _ __   ___| | \n \
-        | . ` |/ _ \\ | | | __| '__| | '_ \\ / _ \\  |  < / _ \\ '__| '_ \\ / _ \\ | \n \
-        | |\\  |  __/ |_| | |_| |  | | | | | (_) | | . \\  __/ |  | | | |  __/ | \n \
-        |_| \\_|\\___|\\__,_|\\__|_|  |_|_| |_|\\___/  |_|\\_\\___|_|  |_| |_|\\___|_|", 461);
-        term_write("v0.1-dev", 8);
-
-        term_write("\n\n", 2);
-        term_write("Hello world", 11);
-    }
-    //? -----------------------------------------
-
-    //TODO: implement user-space (final goal)
-    for (;;) asm("hlt");
-    */
 }
 
 void arch_idle() {
