7d35f1e81feca3057938db44557a7b602f223ee8
rickypurogu <tanas.riccardo03@gmail.com>
2022-06-18 16:01:15 +0200

    Added intptr_t

--- libs/libc/stdint.h
+++ libs/libc/stdint.h
@@ -8,6 +8,7 @@ typedef int                  int32_t;
 typedef unsigned int         uint32_t;
 typedef long                 int64_t;
 typedef unsigned long        uint64_t;
+typedef long                 intptr_t;
 typedef unsigned long        uintptr_t;
 
 #define INT_MAX 2147483647
