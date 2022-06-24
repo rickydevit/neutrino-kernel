22b2674804afb445a77bb2b242af4839a30d7ea5
rickypurogu <tanas.riccardo03@gmail.com>
2022-06-24 11:18:48 +0200

    Minor changes to test2

--- executables/test2.c
+++ executables/test2.c
@@ -11,15 +11,19 @@ int main() {
         .type = IPC_PEEK
     };
     neutrino_ipc(&a);
-    uintptr_t ptr = (uintptr_t)malloc(a.size);
-    a.type = IPC_RECEIVE;
-    a.payload = ptr;
-    neutrino_ipc(&a);
 
-    char buf[128];
-    strf("IPC returned payload at %x size %u. Payload is %c", buf, a.payload, a.size, (char*)a.payload);
-    neutrino_log(&(SCLogArgs){.msg = buf});
-    free((void*)ptr);
+    if (a.size > 0) {
+        uintptr_t ptr = (uintptr_t)malloc(a.size);
+        a.type = IPC_RECEIVE;
+        a.payload = ptr;
+        neutrino_ipc(&a);
+        char buf[128];
+        strf("IPC returned payload at %x size %u. Payload is %c", buf, a.payload, a.size, (char*)a.payload);
+        neutrino_log(&(SCLogArgs){.msg = buf});
+        free((void*)ptr);
+    } else {
+        neutrino_log(&(SCLogArgs){.msg = "No payload from IPC"});
+    }
 
     return 0;
 }
